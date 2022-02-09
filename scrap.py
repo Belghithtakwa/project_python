5aa1f31125041dd6e7ead5f84f960a94df2f8e39
takwa <blgeyatatou@gmail.com>
2022-02-09 19:54:30 +0100

    fix

--- scrap.py
+++ scrap.py
@@ -27,20 +27,42 @@ file.write(f'''<div>''')
 if response is not None:
  html = BeautifulSoup(response.content, 'html.parser')
 
- results = html.find_all("div", class_="content")
+ results = html.find_all("div", class_="content")[2:][2:10]
  for result in results:
+    if result is not None:
      location = result.find("span", class_="new_feature")
      description = result.find("div", class_="c_text_secondary description")
+     description = result.text
+     description = description.replace("é", "e")
+     description = description.replace("à", "a")
+     description = description.replace("ù", "u")
+     description = description.replace("è", "e")
+     description = description.replace("ê", "e")
+     description = description.replace('É', 'E')
+     description = description.replace('ô', 'o')
+     description = description.replace('ç', 'c')
+     description = description.replace('?', ' ')
      date = result.find("span", class_="publication_date recent")
+     if date is not None:
+      date = date.find("span")
+      date = date.text
      title = result.find("span", class_="c_link_annonces")
+     if title is not None:
+      title = title.text
+      title = title.replace("é", "e")
+      title = title.replace('ô', 'o')
+      title = title.replace("à", "a")
      title = re.sub('^null|$','', str(title))
      date = re.sub('^null|$','', str(date))
      description = re.sub('^null|$','', str(description))
      requests.post(url, {"title": " ".join(title.split()),"date": " ".join(date.split()),"description": " ".join(description.split())}, headers)
      file.write(f'''<div class="parag">
- <h5 class="titleJob" >title job: {title} </h5>
-<li><span class="des" > La date de l'annonce :</span>{date}</li>  
-<li><span class="des" > lieu du travail :</span> {location}</li>
+   {f'<h4>{title}</h4>' if title else ""} </br>
+<li><span class="des" > La date de l'annonce :</span>
+ {f'<span>{date}</span>' if date else ""} </br>
+</li>  
+<li><span class="des" > lieu du travail :</span>
+  {f'<span>{location}</span>' if location else ""} </br> 
 <span class="des" >L'annonce : </span> <br>
 <p>{description}</p>
 </div>
